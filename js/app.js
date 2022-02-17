421c35401e5c7a3da49740e48a2ffaf71b740d68
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-17 02:48:44 -0800

    fun & valid added  commit

--- js/app.js
+++ js/app.js
@@ -15,24 +15,70 @@ const calculate = document.getElementById('calculate');
 const save=document.getElementById('savings');
 
 
+//function error
+function err (id){
+    document.getElementById(id).style.display='inline-block';
+}
+
+
+
+//input value function
+function Inputvalue(id){
+    let inputBox =document.getElementById(id);
+    let inputAmount= parseInt(inputBox.value);
+     if(isNaN(inputAmount) || inputAmount< 0){
+         inputBox.value= '';
+         alert('enter value');
+       
+     }
+     else{
+    return inputAmount;
+     }
+}
+
+
+//setText Function
+function setText (id, val){
+   let innerTextId =document.getElementById(id);
+//    let innerTextAmount= parseInt(innerTextId).innerText;
+//    console.log(innerTextAmount);
+
+//        if(isNaN(innerTextAmount) || innerTextAmount < 0){
+//            innerTextId.innerText= 0;
+//        } else{
+//         innerTextId.innerText= innerTextAmount;
+//        }
+   if(isNaN(val) || val < 0){
+    innerTextId.innerText= 0;
+   } else{
+    innerTextId.innerText= val;
+   }
+
+
+     
+}
+
+
 
 //calculation event
 calculate.addEventListener('click', function(){
     let total=0;
     let expense=0;
     
-    const income= document.getElementById('income').value;
-    const food= document.getElementById('food').value;
-    const rent= document.getElementById('rent').value;
-    const clothes=document.getElementById('clothes').value;
+    const income=  Inputvalue('income');
+    const food=  Inputvalue('food');
+    const rent=  Inputvalue('rent');
+    const clothes=  Inputvalue('clothes');
 
     expense= parseInt(food) + parseInt(rent) + parseInt(clothes);
+    console.log(expense);
     
-    document.getElementById('total-expense').innerText=expense;
+    setText('total-expense', expense)
+    
 
     total = income -expense;
 
-    document.getElementById('balance').innerText=total;
+    setText('balance' , total);
 
     
     
@@ -47,18 +93,22 @@ calculate.addEventListener('click', function(){
 save.addEventListener('click', function(){
     let percantage=0;
     let savingBalance=0;
-    const income= document.getElementById('income').value;
-    const save=document.getElementById('save').value;
+    const income=  Inputvalue('income');
+    const save= Inputvalue('save');
 
     percantage= (save * income) / 100;
      
-    document.getElementById('saving-amount').innerText=percantage;
+    
+
+    setText('saving-amount' , percantage);
 
     let balance= document.getElementById('balance').innerText;
 
     savingBalance=balance - percantage;
 
-    document.getElementById('remaining-balance').innerText=savingBalance; 
+    
+
+    setText('remaining-balance', savingBalance)
 
     
    
